ec45ef15bccbf3f30389f1dc2cdbb8e6c2c1e011
Lyle <lyle@watcha.com>
2019-10-01 11:05:23 +0900

    chore(DarkSkyAPI) : 불 필요한 코드 정리

--- Yellow/Networking/DarkSkyAPI.swift
+++ Yellow/Networking/DarkSkyAPI.swift
@@ -14,18 +14,17 @@ enum APIEnvironment {
 }
 
 struct DarkSkyAPI {
-
     // 날씨 정보는 15분 마다 갱신되도록
     static let updateTimeInterval: TimeInterval = 60 * 15
     static let baseURL = "https://api.darksky.net"
     static let key = "9c87420e4d83e34f0d7e8fba0c272c22"
-    static let shared = DarkSkyAPI()
     
     @discardableResult
     static func request(location: WeatherLocation, completion: @escaping (Result<Data, Error>) -> Void) -> Router<DarkSkyAPIEndPoint> {
         request(latitude: location.latitude, longitude: location.longitude, completion: completion)
     }
-
+    
+    @discardableResult
     static func request(latitude: Double, longitude: Double, completion: @escaping (Result<Data, Error>) -> Void) -> Router<DarkSkyAPIEndPoint> {
         let router = Router<DarkSkyAPIEndPoint>()
         router.request(.forecast(latitude: String(latitude), longitude: String(longitude))) { result in
